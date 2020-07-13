3198b868084cd06c5ef898ab6a27974602ab9e54
bibraj96 <blendi.ibraj96@gmail.com>
2020-07-13 16:37:51 -0400

    add comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :username, uniqueness: true, presence: true
+  validates :email, uniqueness: true, presence: true
   #has_secure_password validates our password 
 end
