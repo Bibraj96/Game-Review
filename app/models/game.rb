5a0cdec2e853cd26c4b90b7cbdfca1ab3798367d
bibraj96 <blendi.ibraj96@gmail.com>
2020-07-13 15:06:13 -0400

    add has_many and has many_through associations to necessary models

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,6 @@
 class Game < ApplicationRecord
   belongs_to :genre
-  belongs_to :user #owner of a game, not another join table
+  belongs_to :user #creator of a game, not another join table
+  has_many :reviews
+  has_many :users, through: :reviews #users who have reviewed the games
 end
