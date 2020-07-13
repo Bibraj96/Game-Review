0bb5a8a3b9d22847a87eb2849470a238fe049b0b
bibraj96 <blendi.ibraj96@gmail.com>
2020-07-13 16:18:31 -0400

    add username column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_13_185542) do
+ActiveRecord::Schema.define(version: 2020_07_13_185645) do
 
   create_table "games", force: :cascade do |t|
     t.string "title"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2020_07_13_185542) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
   end
 
   add_foreign_key "games", "genres"
