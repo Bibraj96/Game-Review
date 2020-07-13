7f33595b9a0f8b18396b250e721c8838726ce30c
bibraj96 <blendi.ibraj96@gmail.com>
2020-07-13 16:23:35 -0400

    add has_secure_password to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :reviews
   has_many :reviewed_games, through: :reviews, source: :game #see odinproject site for how to use source in models
-
   has_many :games #these are games that the user created; gives us @user.games
+
+  has_secure_password
 end
