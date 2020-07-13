73d21eb0c5a8ce96a654399285801f766f2d926e
bibraj96 <blendi.ibraj96@gmail.com>
2020-07-13 15:20:23 -0400

    add sessions folder to views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :reviews
-  has_many :reviewed_games, through: :reviews, source: :game
+  has_many :reviewed_games, through: :reviews, source: :game #see odinproject site for how to use source in models
 
   has_many :games #these are games that the user created; gives us @user.games
 end
