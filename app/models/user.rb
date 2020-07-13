f236b69f0414c29df438d4a426317a5f11a2865e
bibraj96 <blendi.ibraj96@gmail.com>
2020-07-13 15:16:33 -0400

    change :games to :reviewed_games using source: in the has_many, through association

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :reviews
-  has_many :games, through: :reviews
+  has_many :reviewed_games, through: :reviews, source: :game
 
   has_many :games #these are games that the user created; gives us @user.games
 end
