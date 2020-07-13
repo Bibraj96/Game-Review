4adb84c796ed1ec2b2080fdcb1c5efab033788d5
bibraj96 <blendi.ibraj96@gmail.com>
2020-07-13 15:58:44 -0400

    add get routes for login and signup, post route for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get '/' => "sessions#welcome"
+  get '/' => 'sessions#welcome'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/signup' => 'users#new'
+  
   resources :games
   resources :reviews
   resources :genres
