7777e3d4ddca8f1adfc77146c76c628d98712cff
bibraj96 <blendi.ibraj96@gmail.com>
2020-07-14 11:35:44 -0400

    add destroy action to delete session and log user out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,11 @@
 class SessionsController < ApplicationController
+  
   def welcome
-    
   end
+
+  def destroy
+    session.delete(:user_id) # #destroy is mainly used for objects
+    redirect_to '/'
+  end
+
 end
