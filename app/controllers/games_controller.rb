bc26df1cd8fbcb4e7e3ffff9119a8941774458d9
bibraj96 <blendi.ibraj96@gmail.com>
2020-07-14 13:38:59 -0400

    create game_params that accept a title and genre name attribute

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,6 +2,13 @@ class GamesController < ApplicationController
 
   def new
     @game = Game.new
+    @game.build_genre
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:title, :genre_attributes [:name])
   end
 
 end
