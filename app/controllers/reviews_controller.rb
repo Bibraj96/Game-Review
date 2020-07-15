15077d66ec939031934430469bb34b6d58996c6e
bibraj96 <blendi.ibraj96@gmail.com>
2020-07-15 12:24:28 -0400

    add review params private method and create method (w/o functionality)

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,4 +8,13 @@ class ReviewsController < ApplicationController
     @review = @game.reviews.build
   end
 
+  def create
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content, :game_id)
+  end
+
 end
