9ba54ebfc99b824fccfd40129eb24a0d9ff68b49
bibraj96 <blendi.ibraj96@gmail.com>
2020-07-15 12:55:17 -0400

    add validations for presence of content and numericality of rating

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :game
+
+  validates :rating, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than: 11}
+  validates :content, presence: true
 end
