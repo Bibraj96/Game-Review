9416f1c3d51517cc582f549e048882c8a53a1cc8
bibraj96 <blendi.ibraj96@gmail.com>
2020-07-16 12:56:34 -0400

    fix syntax for validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,5 @@ class Review < ApplicationRecord
 
   validates :rating, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than: 11}
   validates :content, presence: true
+  validates :game, uniqueness: { scope: :user, message: "Error: You've already reviewed this game!"}
 end
