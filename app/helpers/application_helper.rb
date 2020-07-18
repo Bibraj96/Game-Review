c716b9935ea54d75f2dfef293119c35336364f5d
bibraj96 <blendi.ibraj96@gmail.com>
2020-07-17 20:57:12 -0400

    add navigation method to render correct partial whether the user is logged in or not

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  
+  def navigation
+    if logged_in?
+      render partial: 'layouts/login_navigation'
+    else
+      render partial: 'layouts/logout_navigation'
+    end
+  end
+
 end
