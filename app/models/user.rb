6c652bbedf51f9a521ba3efa4a712c6c687e940c
bibraj96 <blendi.ibraj96@gmail.com>
2020-07-18 16:41:57 -0400

    add class method to find or create user with google

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,12 @@ class User < ApplicationRecord
   validates :username, uniqueness: true, presence: true
   validates :email, uniqueness: true, presence: true
   #has_secure_password validates our password 
+
+  def self.find_or_create_by_google(auth)
+    self.find_or_create_by(username: auth[:info][:email]) do |u| #OR: User.where(email: auth[:info][:email]).first_or_initialize
+      u.email = auth[:info][:email]
+      u.password = SecureRandom.hex #creates a random password
+    end
+  end
+
 end
