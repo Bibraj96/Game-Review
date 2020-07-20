171e157c355473395a9a58a28a7439556e565a90
bibraj96 <blendi.ibraj96@gmail.com>
2020-07-20 12:48:42 -0400

    edit validation syntax

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,6 +1,5 @@
 class Genre < ApplicationRecord
   has_many :games
 
-  validates :name, presence: true
-  validates :name, uniqueness: true
+  validates :name, presence: true, uniqueness: true
 end
