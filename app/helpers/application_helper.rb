67507e93eeacb54605f536c8d7b23a8c49c55519
bibraj96 <blendi.ibraj96@gmail.com>
2020-08-06 20:26:25 -0400

    remove logout conditional from navigation method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,6 @@ module ApplicationHelper
   def navigation
     if logged_in?
       render partial: 'layouts/login_navigation'
-    else
-      render partial: 'layouts/logout_navigation'
     end
   end
 
